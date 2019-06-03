7395821854f12923f99ba75d2441155ff9d26707
Alexandre Simard Forbes <alexandresimardforbes@gmail.com>
2019-06-03 10:13:50 -0400

    Added for loop exercises

---
+++ Exercises/JavaScript/For loops problem set/forloop.js
@@ -0,0 +1,30 @@
+for (let index = -10; index <= 19; index++) {
+    console.log(index)
+    
+}
+
+console.log("------------------------------------------------------------------------")
+
+for (let index = 10; index <= 40; index += 2){
+   
+    console.log(index)
+}
+
+console.log("------------------------------------------------------------------------")
+
+
+for (let index = 300; index <= 333; index++) {
+    if( index % 2 != 0){
+        console.log(index)
+    }
+    
+}
+
+console.log("------------------------------------------------------------------------")
+
+for (let count = 5; count < 50; count++) {
+  
+    if( count % 5 == 0 && count % 3 == 0){
+        console.log(count)
+    }
+}
