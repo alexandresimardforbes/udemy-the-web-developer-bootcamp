6968819107b908e33979e63297a77e6fe5dfebd2
Alexandre Simard Forbes <alexandresimardforbes@gmail.com>
2019-06-06 10:30:13 -0400

    Added Dom selectors

---
+++ Examples/DOM manipulation/DOM Selectors/DomSelectors.js
@@ -0,0 +1,5 @@
+var tag = document.getElementById("highlight");
+var tags = document.getElementsByClassName("bolded");
+var tagsTag = document.getElementsByTagName("li");
+var tagQuery = document.querySelector("#highlight");
+var tagQueryAll = document.querySelectorAll("ul li.bolded");
