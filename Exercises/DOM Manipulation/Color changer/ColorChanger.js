3e44f8a1c82ffa126ac359748c2b421c1f225613
Alexandre Simard Forbes <alexandresimardforbes@gmail.com>
2019-06-06 15:47:39 -0400

    Added moddif

--- Exercises/DOM Manipulation/Color changer/ColorChanger.js
+++ Exercises/DOM Manipulation/Color changer/ColorChanger.js
@@ -1,13 +1,20 @@
-var button = document.querySelector("button");
+var buttons = document.querySelectorAll("button");
 var isPurple = false;
 
-button.addEventListener("click", function() {
+// button.addEventListener("click", function() {
  
-    if (isPurple) {
-        document.body.style.background = "white";
-    }
-    else {
-        document.body.style.background = "purple";
-    }
-    isPurple = !isPurple;
-})
+//     if (isPurple) {
+//         document.body.style.background = "white";
+//     }
+//     else {
+//         document.body.style.background = "purple";
+//     }
+//     isPurple = !isPurple;
+// })
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function () {
+        document.body.classList.toggle("bgColor");
+    });
+ 
+}
