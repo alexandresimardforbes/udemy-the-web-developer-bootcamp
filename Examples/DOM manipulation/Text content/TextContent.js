76b1499ce22a24f3169fe8d67f071d40d77d5a30
Alexandre Simard Forbes <alexandresimardforbes@gmail.com>
2019-06-06 11:15:15 -0400

    Added text contont

---
+++ Examples/DOM manipulation/Text content/TextContent.js
@@ -0,0 +1,4 @@
+var p = document.getElementsByTagName("p")[0];
+var ul = document.querySelector("ul");
+document.querySelector("h1").textContent = "END OF THIS LESSON!";
+document.body.innerHTML = "<h1>Goodbye</h3>"
