b9af0a491e28c0b52543622d4fa489abf4df14c2
Alexandre Simard Forbes <alexandresimardforbes@gmail.com>
2019-06-06 11:59:58 -0400

    Added attributes

---
+++ Examples/DOM manipulation/Attributes/attributes.js
@@ -0,0 +1,8 @@
+var link = document.querySelector("a");
+var img = document.querySelector("img");
+
+link.getAttribute("href");
+link.setAttribute("href","https://www.dog.com")
+
+img.setAttribute("src","https://zeroproject.org/wp-content/uploads/2018/05/Microsoft.png")
+link.textContent = "Link to dog.com"
